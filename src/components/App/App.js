1538c0fa0d99c6238a58d369e05b8ae9cedf3d03
Jorge Valdez <valdez.a.jorge@gmail.com>
2018-03-26 16:08:06 -0500

    moving from hammer cuz it doesnt do what  i need it to

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import Konva from 'konva';
-import Hammer from 'react-hammerjs';
 import './App.css';
 
-const swipeToPoints = (swipe) => {
-  const {
-    srcEvent,
-    deltaTime,
-    overallVelocityX,
-    overallVelocityY,
-    overallVelocity
-  } = swipe;
-
-  // srcEvent is actually the pointerUp moment
-  // I think it might be good to keep this for sound
-  // synthesis but in terms of drawing the actual line
-  // I definitely need to do the built in event handlers
-  const { clientX, clientY } = srcEvent;
-
-  const startPoint = {
-    x: clientX - overallVelocityX * deltaTime,
-    y: clientY - overallVelocityY * deltaTime,
-  };
-
-  const endPoint = {
-    x: startPoint.x + overallVelocityX * deltaTime,
-    y: startPoint.y + overallVelocityY * deltaTime,
-  };
-
-  return [
-    startPoint.x,
-    startPoint.y,
-    endPoint.x,
-    endPoint.y,
-  ];
-};
-
-const SwipeLine = ({ e }) => (
+const PointsLin = ({ points }) => (
   <Line
     stroke={"black"}
-    points={swipeToPoints( e )}
+    points={points}
   />
 );
 
@@ -51,32 +17,32 @@ class App extends Component {
     this.state = {
       touchStart: null,
       currentTouch: null,
-      lastTouchEvent: null,
+      touchEnd: null,
       isTouching: false,
       swipe: null,
       swipePath: null,
     };
   }
 
-  onSwipe = (e) => {
+  onMouseDown = (e) => {
     e.preventDefault();
-    console.dir(e);
-    this.setState({
-      swipe: e,
-    });
-  };
 
-  onPress = (e) => {
-    e.preventDefault();
+    const newTouch = [
+      e.pageX || e.touches[0].pageX,
+      e.pageY || e.touches[0].pageY
+    ];
+
     this.setState({
       isTouching: this.state.isTouching || true,
-      touchStart: this.state.isTouching ? e : this.state.touchStart,
-      currentTouch: e
+      touchStart: this.state.isTouching ?
+                  newTouch : this.state.touchStart,
+      currentTouch: newTouch
     });
   };
 
-  onPressUp = (e) => {
+  onMouseUp = (e) => {
     e.preventDefault();
+
     this.setState({
       isTouching: false,
       currentTouch: null,
@@ -86,20 +52,22 @@ class App extends Component {
 
   render() {
     return (
-      <Hammer
-        className="HammerContainer"
-        onSwipe={this.onSwipe}
-        action={this.onPress}
-        onPressUp={this.onPressUp}>
-        <div>
-          <Stage width={window.innerWidth - 20} height={window.innerHeight - 20}>
-            <Layer>
-              {this.state.swipe ?
-                <SwipeLine e={this.state.swipe} /> : null}
-            </Layer>
-          </Stage>
-        </div>
-      </Hammer>
+      <div className="AppContainer">
+      <Stage
+        onMouseDown={this.onMouseDown}
+        onTouchStart={this.onMouseDown}
+
+        onMouseUp={this.onMouseUp}
+        onTouchEnd={this.onMouseUp}
+
+        width={window.innerWidth - 20}
+        height={window.innerHeight - 20}>
+          <Layer>
+            {this.state.swipe ?
+              <SwipeLine e={this.state.swipe} /> : null}
+          </Layer>
+        </Stage>
+      </div>
     );
   }
 }
